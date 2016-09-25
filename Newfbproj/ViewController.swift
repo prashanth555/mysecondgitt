e8815565719a3af6985207b983a5ac8dc1a820d6
prashnth <prashnthchemalapati@gmail.com>
2016-09-25 15:20:24 -0400

    hello world

---
+++ Newfbproj/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Newfbproj
+//
+//  Created by Prashanth Chemalapati on 9/25/16.
+//  Copyright © 2016 sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+   
+    print("Hello world")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
